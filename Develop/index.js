b72d7209356cc03797d945c27149a99b4b9b130c
Javiistacks <javiann.marrero@icloud.com>
2021-04-03 10:38:21 -0500

    loaded generate and index.js work

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,96 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateMarkdown");
+const axios = require("axios");
+
+const promptUser = () => {
+    return inquirer.prompt(questions)
+  };
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        message: "Enter the Project title?",
+        type: "input",
+        name: "title",
+      },
+      {
+        message: "Enter the GitHub URL for the Project?",
+        type: "input",
+        name: "repoURL",
+      },
+      {
+        message: "Enter a brief Project description?",
+        type: "input",
+        name: "description",
+      },
+      {
+        message: "Enter how to install the Project?",
+        type: "input",
+        name: "installation",
+      },
+      {
+        message: "Enter how to use the Project?",
+        type: "input",
+        name: "usage",
+      },
+      {
+        message: "Choose the license that applies to the Project?",
+        type: "list",
+        choices: ["Apache", "CC", "GPLv3", "GPLv2", "ISC", "MIT", "WTFPL", "None"],
+        name: "license",
+      },
+      {
+        message: "What needs to be known about contributing to the Project?",
+        type: "input",
+        name: "contributing",
+      },
+      {
+        message: "Enter any test instructions for the Project?",
+        type: "input",
+        name: "tests",
+      },
+      {
+        message: "Enter your GitHub Username?",
+        type: "input",
+        name: "username",
+      },
+      {
+        message: "Enter your email address.",
+        type: "input",
+        name: "email",
+      },
+      {
+        message: "What is the copyright year?",
+        type: "input",
+        name: "year",
+      },
+      {
+        message: "Type your full name?",
+        type: "input",
+        name: "name",
+      },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if(err) throw err
+        console.log("Success!")
+      })
+    };
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  promptUser().then(responses => {
+    axios.get("https://api.github.com/users/" + responses.username).then(results => {
+      responses.username = results.data.html_url;
+    const readmeContent = generateReadme(responses)
+      writeToFile("./README.md", readmeContent);
+    })
+  })
+};
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,55 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function generateMarkdown(responses) {
+  return `
+# [${responses.title}](${responses.repoURL})
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+${responses.license === "Apache" ? "![Badge](https://img.shields.io/badge/license-Apache%202.0-f56b02)" : 	
+  responses.license === "CC"     ? "![Badge](https://img.shields.io/badge/license-CC-f2ea05)" : 	
+  responses.license === "GPLv3"  ? "![Badge](https://img.shields.io/badge/license-GPL%20v3-40e637)" : 	
+  responses.license === "GPLv2"  ? "![Badge](https://img.shields.io/badge/license-GPL%20v2-0526fc)" : 	
+  responses.license === "ISC"    ? "![Badge](https://img.shields.io/badge/license-ISC-850fd4)" : 	
+  responses.license === "MIT"    ? "![Badge](https://img.shields.io/badge/license-MIT-f2056c)" : 	
+  responses.license === "WTFPL"  ? "![Badge](https://img.shields.io/badge/license-WTFPL-f20707)" : ""}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+## Description
+${responses.description}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [License](#license)
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+## Installation 👨🏽‍💻 <a name="installation"></a>
+${responses.installation}
 
+## Usage <a name="usage"></a>
+${responses.usage}
+
+## Contributing <a name="contributing"></a>
+${responses.contributing}
+
+## Tests <a name="tests"></a>
+${responses.tests}
+
+
+## Questions <a name="questions"></a>
+
+* If you have any questions please [**eMail**](mailto:${responses.email}) me
+* :octocat: Check out more of my work on [**GitHub**](${responses.username})
+
+
+## License 📝 <a name="license"></a>
+
+This project is ${responses.license === "Apache" ? "**`Apache 2.0`**" : 
+                  responses.license === "CC"     ? "**`Creative Commons`**"  :  
+                  responses.license === "GPLv3"  ? "**`General Public License v3.0`**"  : 	
+                  responses.license === "GPLv2"  ? "**`General Public License v2.0`**"  : 	
+                  responses.license === "ISC"    ? "**`Internet Systems Consortium`**"  : 	
+                  responses.license === "MIT"    ? "**`Massachusetts Institute of Technology`**"  : 	
+                  responses.license === "WTFPL"  ? "**`WTFPL`**"  : "Not"} licensed
+---
+ Copyright ©️ ${responses.year}  ${responses.name}
 `;
 }
 
